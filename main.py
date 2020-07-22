fcee3a5369e72a26e9396aa58f91bd70409caadc
Jancuch <kupaguj@gmail.com>
2020-07-22 14:16:29 +0200

    small fix

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ response = req.get('https://www.reddit.com/', headers={'User-Agent': 'Mozilla/5.
 soup = BeautifulSoup(response.text , 'html.parser' )
 images = soup.findAll('img' , attrs={"alt":"Post image"})
 number = 0 
-bnumber =  "{0}.png".format(number)
+
 for image in images:
     number += 1
+    bnumber =  "{0}.png".format(number)
     download = image['src']
     urllib.request.urlretrieve(download, str(bnumber))
     
