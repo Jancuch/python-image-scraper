98e43c72cc780861d7677c7593ccd953ffea6b1b
Jancuch <kupaguj@gmail.com>
2020-07-22 12:40:22 +0200

    download

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-
+import urllib.request
+import os
 
 req = requests
 
 response = req.get('https://www.reddit.com/', headers={'User-Agent': 'Mozilla/5.0'})
-soup = BeautifulSoup(response.text , 'html.parser')
-images = soup.findAll('img')
+soup = BeautifulSoup(response.text , 'html.parser' )
+images = soup.findAll('img' , attrs={"alt":"Post image"})
+number = 0 
+bnumber =  "{0}.png".format(number)
 for image in images:
-    print( image['src'])
+    number += 1
+    download = image['src']
+    urllib.request.urlretrieve(download, str(bnumber))
+    
+
+    
 
     
