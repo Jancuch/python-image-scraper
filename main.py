3b9e719968e2c1d4d104a25f7f8fdf45dc12e541
Jancuch <kupaguj@gmail.com>
2020-07-23 10:59:47 +0200

    function

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import urllib.request
 import os
 
 req = requests
+def scrape(link):
+    response = req.get(link , headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text , 'html.parser' )
+    images = soup.findAll('img' , attrs={"alt":"Post image"})
+    number = 0 
 
-response = req.get('https://www.reddit.com/', headers={'User-Agent': 'Mozilla/5.0'})
-soup = BeautifulSoup(response.text , 'html.parser' )
-images = soup.findAll('img' , attrs={"alt":"Post image"})
-number = 0 
-
-for image in images:
-    number += 1
-    bnumber =  "./data/images/{0}.png".format(number)
-    download = image['src']
-    urllib.request.urlretrieve(download, str(bnumber))
+    for image in images:
+        number += 1
+        bnumber =  "./data/images/{0}.png".format(number)
+        download = image['src']
+        urllib.request.urlretrieve(download, str(bnumber))
+        
+scrape('https://www.reddit.com/r/tf2memes/')
+    
     
 
     
